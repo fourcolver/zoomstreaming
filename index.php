12ebe2b82dc306d5576d31846d87f571fc4d3ac4
medusa628 <medusa628@yandex.com>
2020-10-18 10:30:48 -0700

    second

--- index.php
+++ index.php
@@ -7,8 +7,13 @@
 <body>
 	<div class="container" style="margin-top: 100px">
 		<div class="iframe-container" style="overflow: hidden; padding-top: 56.25%; position: relative;">
-	    	<iframe allow="microphone; camera" style="border: 0; height: 100%; left: 0; position: absolute; top: 0; width: 100%;" src="https://zoom.us/wc/7213533296/join?prefer=1&un=bWFzdGVyZGV2IGNsb3Zlcg==" frameborder="0" sandbox="allow-forms allow-scripts allow-same-origin" allow="microphone; camera"></iframe>
+	    	<!-- <iframe allow="microphone; camera" style="border: 0; height: 100%; left: 0; position: absolute; top: 0; width: 100%;" src="https://zoom.us/wc/7213533296/join?prefer=1&un=bWFzdGVyZGV2IGNsb3Zlcg==" frameborder="0" sandbox="allow-forms allow-scripts allow-same-origin" allow="microphone; camera"></iframe> -->
+	    	<iframe src="https://zoom.us/wc/7213533296/join?prefer=1&un=bWFzdGVyZGV2IGNsb3Zlcg==&pwd=c2swb4" style="border: 0; height: 100%; left: 0; position: absolute; top: 0; width: 100%;" sandbox="allow-forms allow-scripts allow-same-origin" allow="microphone; camera; fullscreen"></iframe>
 		</div>
+		<!-- <div>
+			https://zoom.us/wc/7213533296/join?prefer=1&un=TWluZGF1Z2Fz&pwd=c2swb4
+		</div> -->
+
 		<!-- <div class="iframe-container" style="overflow: hidden; padding-top: 56.25%; position: relative;">
 	    	<iframe allow="microphone; camera" style="border: 0; height: 100%; left: 0; position: absolute; top: 0; width: 100%;" src="https://us05web.zoom.us/wc/7213533296/start" frameborder="0"></iframe>
 		</div> -->
